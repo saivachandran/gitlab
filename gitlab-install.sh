0efbe0ffe286b5965c132bef9881f605ed4d76f7
saivachandran <chandransaiva@gmail.com>
2021-11-19 07:19:15 +0000

    install

---
+++ gitlab-install.sh
@@ -0,0 +1,23 @@
+## cat gitlab_install.sh 
+
+#!/bin/bash
+sudo docker run --detach \
+  --hostname gitlab.example.com \
+  --publish 10.128.0.5:443:443 \
+  --publish 10.128.0.5:80:80 \
+  --publish 10.128.0.5:2222:22 \
+  --name gitlab \
+  --restart always \
+  --volume $GITLAB_HOME/config:/etc/gitlab \
+  --volume $GITLAB_HOME/logs:/var/log/gitlab \
+  --volume $GITLAB_HOME/data:/var/opt/gitlab \
+  gitlab/gitlab-ee:latest
+
+
+## reset git root password in docker container
+
+# inside container shell
+
+sudo gitlab-rake "gitlab:password:reset[root"
+
+
